14d1dd7cca3ba3b9faba4cdb38430f4ab3ac944f
Ausubel Velasquez <intelectus61@gmail.com>
2022-08-07 17:19:19 -0500

    Cambiando casi todo

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 from random import randint
+
 def getWord():
     with open("./Data/palabras.txt","r") as f:                
         a = [i for i in f]
     return a.pop(randint(0, len(a)-1)).replace("\n", "")
 
-def run():
-    points = 0
-    wordInGame = "ISABOTH"
-    wordInGameList = [i for i in wordInGame]
-    wordInGameUnderscore = str("___ "*len(wordInGame)) 
 
-    lock = True
-    letras = []
+def getSyllable():
     while True:
-        aux = input("Ingrese la letra: ")
-        letras.append(aux)
-        if aux.isalpha():
+        sil = input("Ingrese la letra: ").upper()
+        if sil.isalpha():
             break        
         else:
             print("Ingrese un valor del alfabeto")
-    for i in wordInGameList:        
-        print(wordInGameList.index(i))
-        print(i,end=" ")
-    
+    return sil
+
+
+def run():
+    points = 0
+    wordInGame = "ELPEPE"
+    wordInGameList = [i for i in wordInGame]    
+    wordInGameUnderscore = ["___" for i in wordInGame]
 
     
-
-
-
 
 if __name__=='__main__':
     run()
